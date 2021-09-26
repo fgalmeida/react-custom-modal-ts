7b69c3cd398026d66f64338467f67a9e7b79aa1a
Felipe Almeida <felipegoa@hotmail.com>
2021-09-26 04:28:23 -0300

    Updates

---
+++ precache-manifest.ba62bf23aa73abe5ce4320560d9958a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f29de3c7c7e3b410318771594385874e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d3cdc0415f6e7793bab6",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "b67bc8e5b25e2397b65c",
+    "url": "./static/js/2.8a8b3683.chunk.js"
+  },
+  {
+    "revision": "0e77ccc9007464ffd437757d6ef9c957",
+    "url": "./static/js/2.8a8b3683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3cdc0415f6e7793bab6",
+    "url": "./static/js/main.5f199e1c.chunk.js"
+  },
+  {
+    "revision": "c79e86135e8f9a125d68",
+    "url": "./static/js/runtime-main.1da8b6d4.js"
+  }
+]);
